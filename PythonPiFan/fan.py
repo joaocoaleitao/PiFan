3033ed7c670a0b59fb4b788c06fd83c2033f31fa
João Leitão <joaocoaleitao@gmail.com>
2022-02-05 13:04:43 +0000

    Latest code on python script.

--- PythonPiFan/fan.py
+++ PythonPiFan/fan.py
@@ -1,3 +1,5 @@
+import logging
+import logging.handlers
 from gpiozero import PWMOutputDevice, CPUTemperature
 import configparser
 import time
@@ -31,6 +33,7 @@ class PiFan(object):
         self.minimum_fan = int(cfg['MAIN']['MinimumFanSpeed'])/100
         self.maximum_fan = int(cfg['MAIN']['MaximumFanSpeed'])/100
         self.debug = int(cfg['MAIN']['Debug'])
+        self.log_delay = int(cfg['MAIN']['LogDelay'])
 
     def get_fan_value_from_temp(self):
         if self.temp.value < self.minimum_fan:
@@ -40,18 +43,29 @@ class PiFan(object):
         return self.temp.value
 
 if __name__ == '__main__':
+    formatter = logging.Formatter('%(name)s - %(message)s')
+    pifan_logger = logging.getLogger('PiFanLogger')
+    pifan_logger.setLevel(logging.DEBUG)
+    handler = logging.handlers.SysLogHandler(address = '/dev/log')
+    handler.setFormatter(formatter)
+    pifan_logger.addHandler(handler)
+
     pifan = PiFan()
     pifan.read_settings()
     if pifan.debug == 1:
-        print("Current Settings:")
-        print("Fan Update Interval Speed:" + str(pifan.interval))
-        print("Maximum Temperature:" + str(pifan.maximum_temperature))
-        print("Minimum Temperature:" + str(pifan.minimum_temperature))
-        print("Maximum Fan Speed:" + str(pifan.maximum_fan*100))
-        print("Minimum Fan Speed:" + str(pifan.minimum_fan*100))
+        pifan_logger.info("Current Settings:")
+        pifan_logger.info("Log Update Interval:" + str(pifan.log_delay))
+        pifan_logger.info("Fan Update Interval Speed:" + str(pifan.interval))
+        pifan_logger.info("Maximum Temperature:" + str(pifan.maximum_temperature))
+        pifan_logger.info("Minimum Temperature:" + str(pifan.minimum_temperature))
+        pifan_logger.info("Maximum Fan Speed:" + str(pifan.maximum_fan*100))
+        pifan_logger.info("Minimum Fan Speed:" + str(pifan.minimum_fan*100))
+    
+    first_timestamp = 0
     while 1:
         time.sleep(pifan.interval)
         pifan.fan_value(pifan.get_fan_value_from_temp())
         if pifan.debug == 1:
-            print("Current Temperature " + str(round(pifan.temp.temperature,1)) + " | Current Speed " + str(int(pifan.get_fan_value_from_temp() *100))+ "%")
-        
+            if time.time() > first_timestamp + pifan.log_delay:
+                pifan_logger.info("Current Temperature " + str(round(pifan.temp.temperature,1)) + " | Current Speed " + str(int(pifan.get_fan_value_from_temp() *100))+ "%")
+                first_timestamp = time.time()
